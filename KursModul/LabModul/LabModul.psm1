7ebf63deaa4b177105166ab0324e050a84528243
Stefan Ober <StefanO@ppedv.de>
2024-07-19 11:33:49 +0200

    BeginProcessEnd / Pipelinefähigkeit

--- KursModul/LabModul/LabModul.psm1
+++ KursModul/LabModul/LabModul.psm1
@@ -2,22 +2,37 @@
 {
 [cmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$true, 
+               ValueFromPipeline = $true,
+               ValueFromPipelineByPRopertyName = $true)]
     [Validatelength(2,256)]
-    [string]$Text 
+    [string]$Name 
 )
-    $color = 0
-
-    for($i = 0; $i -lt $Text.Length; $i++)
+    Begin
     {
-        $color++
+        Write-Verbose -Message "Wird einmal am Start ausgeführt, zb. Zum öffnen von RemoteVerbindungen"
+    }
+	Process
+    {
+        Write-Verbose -Message "Wird für jedes übergebene Objekt ausgeführt"
+        $color = 0
 
-        Write-Host -Object $Text[$i] -NoNewline -ForegroundColor $color
-        
-        if($color -ge 15)
+        for($i = 0; $i -lt $Name.Length; $i++)
         {
-            $color = 0
+            $color++
+
+            Write-Host -Object $Name[$i] -NoNewline -ForegroundColor $color
+        
+            if($color -ge 15)
+            {
+                $color = 0
+            }
         }
+        Write-Host -Object ""
+    }
+    End
+    {
+        Write-Verbose -Message "Wird einmal am Ende ausgeführt. zb. zum schließen von Remote Verbindungen "
     }
 }
 
