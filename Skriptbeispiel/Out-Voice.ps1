9800330eea55695e5f3895dd08b1f92950af01a1
Stefan Ober <StefanO@ppedv.de>
2024-07-19 14:42:22 +0200

    .Net in PS

---
+++ Skriptbeispiel/Out-Voice.ps1
@@ -0,0 +1,26 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName = $true)]
+[string]$Text
+)
+
+Begin
+{
+    Add-Type -AssemblyName System.Speech
+    $speaker = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+
+    $audiosrv = Get-Service -Name Audiosrv
+    if($audiosrv.Status -ne "Running")
+    {
+        $audiosrv.Start()
+    }
+}
+Process
+{
+    $speaker.Rate = -5
+    $speaker.Speak($Text)
+}
+End
+{
+
+}
